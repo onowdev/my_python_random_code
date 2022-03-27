7d662cdbdbef6b87e9ba53b711bf49af57b8f3b6
onowdev <onowdev@gmail.com>
2022-03-27 20:27:55 +0700

    pass generator

---
+++ pass_generator.py
@@ -0,0 +1,18 @@
+import string
+import random
+
+s1 = string.ascii_lowercase
+s2 = string.ascii_uppercase
+s3 = string.digits
+s4 = string.punctuation
+
+plen = int(input("Enter Password length: "))
+s = []
+
+s.extend(list(s1))
+s.extend(list(s2))
+s.extend(list(s3))
+s.extend(list(s4))
+
+print("Your password: ", end="")
+print("".join(random.sample(s, plen)))
